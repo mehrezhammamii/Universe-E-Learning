fd34b7348c574d1debb973c53820a0522cf9f5a8
mehrez <mehrezhammami3@gmail.com>
2024-06-24 20:26:01 +0100

    d

--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -1,6 +1,6 @@
 const express=require("express"); 
 const studentController=require("../controllers/studentController");
 const studentRouter=express.Router();
-studentRouter.post("/registor",studentController.registerStudent);
+studentRouter.post("/register",studentController.registerStudent);
 studentRouter.post("/login",studentController.loginStudent);
 module.exports=studentRouter;
