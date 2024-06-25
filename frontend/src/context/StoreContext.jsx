d9262dbc348f32e0da0601934d94bea8d49ba8a7
omarNaifer12 <o.naifer7@gmail.com>
2024-06-25 22:10:41 +0100

    done some functionality in front

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,24 +1,59 @@
 import React,{createContext, useEffect, useState} from 'react'
+import axios from 'axios';
 export const StoreContext=createContext(null);
 const StoreContextPrivider = ({children}) => {
  const [token,setToken]=useState("");
  const url="http://localhost:3000";
- useEffect(()=>{
+ const[scoreStudent,setScoreStudent]=useState({})
     
-const laodData=()=>{
-    if(localStorage.getItem("token")){
-  setToken(localStorage.getItem("token"));      
+  const [coursList,setCoursList]=useState([]);
+  const addToScore= async (coursId)=>{
+if(token){
+  await axios.post(url+"/api/score/add",{coursId},{headers:{token}})
+}
+  }
+
+    
+          const fetchCoursList=async ()=>{
+              const response=await axios.get(url+"/api/course")
+              setCoursList(response.data);
+              }
+              const loadScoreStudent=async(token)=>{
+const response= await axios.get(url+"/api/score/get",{headers:{token}});
+
+setScoreStudent(response.data.scoreStudent);
+console.log("scoredata is",response.data.scoreStudent);
+
+         
+}
+
+
+
+
+
+ useEffect(()=>{
+ 
+const laodData=async ()=>{
+  fetchCoursList();
+  const tokenStudent=localStorage.getItem("token");
+    if(tokenStudent){
+  setToken(tokenStudent);      
+ await loadScoreStudent(tokenStudent);
 console.log("token of student login is ",localStorage.getItem("token"));    
 }
 
 }
 laodData();
+console.log("scorestudent",scoreStudent);
 },[])
 
  const contextValue={
     token,
     setToken,
     url,
+    coursList,
+    scoreStudent,
+    setScoreStudent,
  }
     return (
     <StoreContext.Provider value={contextValue}>
